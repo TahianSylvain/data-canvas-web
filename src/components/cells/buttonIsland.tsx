644a03306e1703f8ff332055bcc37fa1f01f940b
Noot-heen <rakotsolnotahiana@gmail.com>
2025-03-17 02:07:44 +0300

    fix: suppression une ligne d'import orphelin qui faisait crash le projet

--- src/components/cells/buttonIsland.tsx
+++ src/components/cells/buttonIsland.tsx
@@ -1,9 +1,9 @@
-import "./buttonIsland.css"
 import Button from "../buttons/buttons"
 
 export default function ButtonIsland() {
+    // TODO: ajout border sur le div wrapper et padding de 2 px un peu a gouche a droite
     return <div className="flex justify-between w-64 h-8">
-
+        {/* This is the div wrapper */}
         <Button icon="arrow_upward"></Button>
         <Button icon="arrow_downward"></Button>
         <Button icon="star"></Button>
