cab74cbdb75155af4ea89d2851bd4dabcf33e111
Noot-heen <rakotsolnotahiana@gmail.com>
2025-03-17 01:26:47 +0300

    fix: rebasé globals.css a partir de la branche buttons feat: modifications du css et du composant relatif aux boutons feat: mise en place du squelette des components des cells

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -1,19 +1,36 @@
 import "./buttons.css"
 import Link from "next/link"
 
-export default function Button({children, color="", icon="", style="", href=""}){
+interface ButtonProps {
+    // voir https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/basic_type_example#useful-react-prop-type-examples
+    children?: React.ReactNode,
+    color?: string,
+    icon?: string,
+    style?: string,
+    href?: string,
+    additionalCSS?: string
+}
 
+export default function Button({children, color="", icon="", style="", href="", additionalCSS=""} : ButtonProps){
+    // additionalCSS: to override the actual hardcoded CSS built into "./buttons.css"
 
     const c = color ? " btn-" + color : ""
     const t = style ? " btn-style-" + style : ""
 
-    const class_name = "btn" + c + t
+    const hasChildren = children ? true : false
+    const buttonRectifierCSS = hasChildren ? null : "btn-textless"
+    const iconRectifierCSS = hasChildren ? null : "btn-icon-textless"
+    const materialRectifierCSS = hasChildren ? null : "material-icons-round-textless"
+    
+    const class_name = "btn " + c + t + additionalCSS + buttonRectifierCSS
 
     const btn_content = <>
-        <span className="btn-icon material-icons-round">{icon}</span>
-        <span>
-            {children}
-        </span>
+        {/* <span className="btn-icon material-icons-round">{icon}</span> */}
+        <span className={`btn-icon ${iconRectifierCSS} material-icons-round ${materialRectifierCSS}`}>{icon}</span>
+        {   hasChildren && <span >
+                {children}
+            </span>
+        }
     </>
     
     // If href not null <a> else <button>
