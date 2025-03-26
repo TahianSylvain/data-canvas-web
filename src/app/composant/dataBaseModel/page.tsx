88473d3d787108a7d75ec934a78b13c542cd5833
Cesar <cesarm10102003@gmail.com>
2025-03-26 18:25:33 +0300

    Model de donner afficher dans la page d'aceuil

--- src/app/composant/dataBaseModel/page.tsx
+++ src/app/composant/dataBaseModel/page.tsx
@@ -1,5 +1,18 @@
+import "./modelData.css"
+import { IoMdAdd } from "react-icons/io";
+
 export default function model_data() {
     return (
-        <div className="data_model"></div>
+        <section className="contain_model_test">
+            <div className="data_model">
+                <div className="contain_icon">
+                    <IoMdAdd size={40}/>
+                </div>
+                <div className="contain_text_1">
+                    <p className="name_base">Motorcycles database</p>
+                    <span className="sous_text">Database</span>
+                </div>
+            </div>
+        </section>
     );
 }
