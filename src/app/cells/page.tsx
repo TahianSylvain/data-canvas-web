f851ac44cf4f2cfa7ca225ee5accdee06f48e1c5
Cesar <cesarm10102003@gmail.com>
2025-07-14 21:50:50 +0300

    cells page test

---
+++ src/app/cells/page.tsx
@@ -0,0 +1,99 @@
+// pages/cells.tsx
+'use client'
+import { useState } from "react";
+import dynamic from "next/dynamic";
+import { python } from "@codemirror/lang-python";
+import { Plus } from "lucide-react";
+import Header from "@/components/header/header";
+
+const CodeMirror = dynamic(() => import("@uiw/react-codemirror"), { ssr: false });
+
+export default function Cells() {
+  const [code, setCode] = useState<string>("print('Hello World')");
+  const [output, setOutput] = useState<string>("");
+  const [cels, setCels] =  useState(
+    [
+        {
+            "input": "string",
+            "outputs": [
+                {
+                "type": "string",
+                "value": "string"
+                }
+            ]
+        },
+    ]
+);
+
+
+//   const handleRun = async () => {
+//     try {
+//       const res = await fetch("/api/run", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({ code }),
+//       });
+
+//       const data = await res.json();
+//       setOutput(data.output || "Aucune sortie");
+//     } catch (err) {
+//       setOutput("Erreur d'exécution");
+//     }
+//   };
+const MoreCels = () => {
+  const newCell = {
+    input: "",
+    outputs: [
+      {
+        type: "string",
+        value: "",
+      },
+    ],
+  };
+
+  setCels([...cels, newCell]);
+};
+
+console.log(cels.length)
+  return (
+    <>
+        <Header />
+    
+        <div className="p-6 max-w-4xl mx-auto">
+            <button onClick={MoreCels} className="btnMoreCels">
+                <Plus size={20} />
+                more cells
+            </button>
+
+        </div>
+        {cels.map((cel, index) => (
+        <div key={index} className="p-6 max-w-4xl mx-auto space-y-4">
+            <h1 className="text-2xl font-bold">Bloc Cellulaire - Éditeur Python</h1>
+            <p className="text-xs text-gray-500">Cell : {index}</p>
+
+            <div className="border rounded p-4 shadow bg-gray-100">
+            <CodeMirror
+                value={code}
+                height="200px"
+                extensions={[python()]}
+                onChange={(value) => setCode(value)}
+            />
+            <button
+                // onClick={handleRun}
+                className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+            >
+                Exécuter
+            </button>
+            </div>
+
+            <div className="border rounded p-4 shadow bg-white">
+            <h2 className="text-lg font-semibold">Résultat :</h2>
+            <pre className="mt-2 whitespace-pre-wrap text-sm">{output}</pre>
+            </div>
+
+            {/* Facultatif : debug */}
+        </div>
+        ))}
+    </>
+  );
+}
